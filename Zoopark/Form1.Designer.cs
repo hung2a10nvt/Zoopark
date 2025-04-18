88cc0c4bcd479880ca2769def68dba1651fdab60
Luu Tuan Hung <hung2a10@gmail.com>
2025-04-18 17:28:04 +0700

    Add project files.

---
+++ Zoopark/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Zoopark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.edInvestment = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.edAds = new System.Windows.Forms.NumericUpDown();
+            this.edVisitor = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.edService = new System.Windows.Forms.NumericUpDown();
+            this.edRevenue = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edInvestment)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edVisitor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edService)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edRevenue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.edInvestment);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.edAds);
+            this.panel1.Controls.Add(this.edVisitor);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.bt1);
+            this.panel1.Controls.Add(this.edService);
+            this.panel1.Controls.Add(this.edRevenue);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1019, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // edInvestment
+            // 
+            this.edInvestment.Location = new System.Drawing.Point(149, 71);
+            this.edInvestment.Name = "edInvestment";
+            this.edInvestment.Size = new System.Drawing.Size(120, 20);
+            this.edInvestment.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Инвенстиции";
+            // 
+            // edAds
+            // 
+            this.edAds.Location = new System.Drawing.Point(470, 49);
+            this.edAds.Name = "edAds";
+            this.edAds.Size = new System.Drawing.Size(120, 20);
+            this.edAds.TabIndex = 8;
+            // 
+            // edVisitor
+            // 
+            this.edVisitor.Location = new System.Drawing.Point(470, 21);
+            this.edVisitor.Name = "edVisitor";
+            this.edVisitor.Size = new System.Drawing.Size(120, 20);
+            this.edVisitor.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(316, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Посетители";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(316, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Рекламный бюджет";
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(648, 21);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(95, 36);
+            this.bt1.TabIndex = 4;
+            this.bt1.Text = "Simulate";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // edService
+            // 
+            this.edService.Location = new System.Drawing.Point(149, 44);
+            this.edService.Name = "edService";
+            this.edService.Size = new System.Drawing.Size(120, 20);
+            this.edService.TabIndex = 3;
+            // 
+            // edRevenue
+            // 
+            this.edRevenue.Location = new System.Drawing.Point(149, 21);
+            this.edRevenue.Name = "edRevenue";
+            this.edRevenue.Size = new System.Drawing.Size(120, 20);
+            this.edRevenue.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Дополнительные услуги";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Доход";
+            // 
+            // chart
+            // 
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(39, 106);
+            this.chart.Name = "chart";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsValueShownAsLabel = true;
+            series1.LabelFormat = "C";
+            series1.Legend = "Legend1";
+            series1.Name = "Доход";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.LabelBorderWidth = 3;
+            series2.Legend = "Legend1";
+            series2.Name = "Дополнительные услуги (еда, напитки)";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Инвенстиции";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Количество посетителей";
+            series5.BorderWidth = 3;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Рекламный бюджет";
+            series6.BorderWidth = 3;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Качество инфраструктуры";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Series.Add(series3);
+            this.chart.Series.Add(series4);
+            this.chart.Series.Add(series5);
+            this.chart.Series.Add(series6);
+            this.chart.Size = new System.Drawing.Size(751, 461);
+            this.chart.TabIndex = 1;
+            this.chart.Text = "chart";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 592);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edInvestment)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edVisitor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edService)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edRevenue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown edService;
+        private System.Windows.Forms.NumericUpDown edRevenue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.NumericUpDown edAds;
+        private System.Windows.Forms.NumericUpDown edVisitor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown edInvestment;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Zoopark/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Zoopark
+{
+    public partial class Form1 : Form
+    {
+        private List<double> revenueData = new List<double>();
+        private List<double> serviceData = new List<double>();
+        private List<double> investmentData = new List<double>();
+        private List<int> visitorData = new List<int>();
+        private List<double> adsData = new List<double>();
+        private List<double> infrastructureData = new List<double>();
+
+        private int currentPeriod = 0;
+        private const int totalPeriods = 12;
+        private double currentRevenue, currentService, currentInvestment, currentAds, currentInfrastructure;
+        private int currentVisitors;
+        private Timer simulationTimer;
+        public Form1()
+        {
+            InitializeComponent();
+
+            simulationTimer = new Timer();
+            simulationTimer.Interval = 500; 
+            simulationTimer.Tick += SimulationTimer_Tick;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            ClearData();
+
+            currentRevenue = (double)edRevenue.Value;
+            currentService = (double)edService.Value;
+            currentInvestment = (double)edInvestment.Value;
+            currentVisitors = (int)edVisitor.Value;
+            currentAds = (double)edAds.Value;
+            currentInfrastructure = 5.0;
+
+            currentPeriod = 0;
+            simulationTimer.Start();
+        }
+
+        private void SimulationTimer_Tick(object sender, EventArgs e)
+        {
+            if (currentPeriod >= totalPeriods)
+            {
+                simulationTimer.Stop();
+                return;
+            }
+
+            UpdateSimulationData(currentPeriod,
+                ref currentRevenue,
+                ref currentService,
+                ref currentInvestment,
+                ref currentVisitors,
+                ref currentInfrastructure,
+                ref currentAds);
+
+            revenueData.Add(currentRevenue);
+            serviceData.Add(currentService);
+            investmentData.Add(currentInvestment);
+            visitorData.Add(currentVisitors);
+            adsData.Add(currentAds);
+            infrastructureData.Add(currentInfrastructure);
+
+            chart.Series[0].Points.AddY(currentRevenue);
+            chart.Series[1].Points.AddY(currentService);
+            chart.Series[2].Points.AddY(currentInvestment);
+            chart.Series[3].Points.AddY(currentVisitors);
+            chart.Series[4].Points.AddY(currentAds);
+            chart.Series[5].Points.AddY(currentInfrastructure);
+
+            chart.Series[0].Points[currentPeriod].AxisLabel = (currentPeriod + 1).ToString();
+
+            currentPeriod++;
+        }
+
+        private void UpdateSimulationData(int period,
+            ref double revenue,
+            ref double service,
+            ref double investment,
+            ref int visitors,
+            ref double infrastructure,
+            ref double ads)
+        {
+            ads = revenue * 0.1;
+            infrastructure = infrastructure * 0.9 + investment * 0.1;
+
+            double maxVisitors = 1000;
+            visitors = (int)Math.Min(maxVisitors, visitors * 0.95 + infrastructure * 3 + ads * 0.1);
+            service = visitors * 2.5 + revenue * 0.05;
+            investment = ads * 0.3 + revenue * 0.12;
+            revenue = revenue * 0.4 + visitors * 2.5 + service * 0.4;
+        }
+
+        private void ClearData()
+        {
+            revenueData.Clear();
+            serviceData.Clear();
+            investmentData.Clear();
+            visitorData.Clear();
+            adsData.Clear();
+            infrastructureData.Clear();
+
+            foreach (var series in chart.Series)
+            {
+                series.Points.Clear();
+            }
+        }
+
+    }
+}
